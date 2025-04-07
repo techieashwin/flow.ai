cc5334b3f24e9b13c679a8e54d7648c1fc0b48ce
techieashwin <techieashwin2004@gmail.com>
2025-04-07 12:55:40 +0530

    some chnages

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,59 +2,63 @@ import React, { useContext, useState } from "react";
 import "./sidebar.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
 import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
-  const{onSent,prevPrompts,setRecentPrompt,newChat} = useContext(Context)
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
 
-  const loadPrompt = async (prompt)=>{
-      setRecentPrompt(prompt)
-      await onSent(prompt)
-  }
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
   return (
     <div className="sidebar">
       <div className="top">
-        <i onClick={()=>setExtended(prev=>!prev)}  class="ri-menu-line"></i>
-        <div onClick={()=>newChat()} className="new-chat">
-          <img  src={assets.plus_icon} alt="" />
+        <i onClick={() => setExtended((prev) => !prev)} className="ri-menu-line"></i>
+        <div onClick={() => newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            {
-              prevPrompts.map((item,index)=>(
-                <div onClick={()=>loadPrompt(item)} className="recent-entry">
+            {prevPrompts.map((item, index) => (
+              <div
+                onClick={() => loadPrompt(item)}
+                className="recent-entry"
+                key={index}
+              >
                 <img src={assets.message_icon} alt="" />
-                <p>{item.slice(0,18)}...</p>
+                <p>{item.slice(0, 18)}...</p>
               </div>
-              ))
-            }
+            ))}
           </div>
         ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
-          <i class="ri-question-line"></i>
-          {extended? <p>Help</p>:null}
+          <i className="ri-question-line"></i>
+          {extended ? <p>Help</p> : null}
         </div>
         <div className="bottom-item recent-entry">
-          <i class="ri-history-line"></i>
-          {extended?<p>Activity</p>:null}
+          <i className="ri-history-line"></i>
+          {extended ? <p>Activity</p> : null}
         </div>
         <div className="bottom-item recent-entry">
-        <i class="ri-settings-3-line"></i>
-          {extended?<p>Settings</p>:null}
+          <i className="ri-settings-3-line"></i>
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
       <div className="app">
-          <Link to='https://flowwordtopdf.netlify.app/' className="card">
-            <img src={assets.word} alt="" />
-          </Link>
-          <Link to='https://filecompress.netlify.app/' className="card">
-            <img src={assets.pdf} alt="" />
-          </Link>
+        <Link to="https://flowwordtopdf.netlify.app/" className="card">
+          <img src={assets.word} alt="" />
+        </Link>
+        <Link to="https://filecompress.netlify.app/" className="card">
+          <img src={assets.pdf} alt="" />
+        </Link>
       </div>
     </div>
   );
